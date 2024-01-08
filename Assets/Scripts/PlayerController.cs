c1fc54e61041a398c9ba79fab8f48e7546ad372e
KTK-Jadoo <kartikeya.jadoo@gmail.com>
2024-01-08 07:07:31 -0800

    Movement and placement.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Camera gameCamera;
+    public Transform objectToPlace;
+    Ray ray;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 displacement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        
+        Vector3 velocity = displacement.normalized * speed;
+
+        transform.Translate(velocity * Time.deltaTime);
+
+        ray = gameCamera.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            objectToPlace.transform.SetPositionAndRotation(hitInfo.point, Quaternion.FromToRotation(Vector3.up, hitInfo.normal));
+        }
+
+
+    }
+}
